7efdf43f69928c2259c450fa2573c448142690fb
adisbladis <adisbladis@gmail.com>
2023-11-18 17:21:09 +1300

    Fix some wrong hook output

--- hooks/node-modules-hook.sh
+++ hooks/node-modules-hook.sh
@@ -1,9 +1,8 @@
 linkNodeModulesHook() {
-    echo "Executing linkNodeModulesShellHook"
+    echo "Executing linkNodeModulesHook"
     runHook preShellHook
 
     @nodejs@ @script@ @storePrefix@ "${nodeModules}/node_modules"
-    ls -la node_modules
     if test -f node_modules/.bin; then
         export PATH=$(readlink -f node_modules/.bin):$PATH
     fi
