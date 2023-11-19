86c1324c7f67584728d1bb0bcb14617948ae3360
adisbladis <adisbladis@gmail.com>
2023-11-19 23:07:44 +1300

    Reduce closure size by reinstating the original package & lock files

--- hooks/npm-config-hook.sh
+++ hooks/npm-config-hook.sh
@@ -9,7 +9,6 @@ npmConfigHook() {
 
     echo "Configuring npm"
 
-    export dontNpmPrune=true  # Opt out of pruning behaviour used by fetchNpmDeps
     export HOME="$TMPDIR"
     export npm_config_nodedir="@nodeSrc@"
     export npm_config_node_gyp="@nodeGyp@"
@@ -18,8 +17,15 @@ npmConfigHook() {
 
     echo "Installing patched package.json/package-lock.json"
 
-    cp --no-preserve=mode "${nodeModules}/package.json" "package.json"
-    cp --no-preserve=mode "${nodeModules}/package-lock.json" "package-lock.json"
+    # Save original package.json/package-lock.json for closure size reductions.
+    # The patched one contains store paths we don't want at runtime.
+    cp --no-preserve=mode package.json "$TMPDIR/package.json"
+    if test -f package-lock.json; then
+        # Not all packages have package-lock.json.
+        cp --no-preserve=mode package-lock.json "$TMPDIR/package-lock.json"
+    fi
+    cp -f --no-preserve=mode "${nodeModules}/package.json" "package.json"
+    cp -f --no-preserve=mode "${nodeModules}/package-lock.json" "package-lock.json"
 
     echo "Installing dependencies"
 
@@ -42,6 +48,12 @@ npmConfigHook() {
 
     # Canonicalize symlinks from relative paths to the Nix store.
     node @canonicalizeSymlinksScript@ @storePrefix@
+
+    # Revert to pre-patched package.json/package-lock.json for closure size reductions
+    cp "$TMPDIR/package.json" package.json
+    if test -f "$TMPDIR/package-lock.json"; then
+        cp "$TMPDIR/package-lock.json" package-lock.json
+    fi
 
     if [ -n "${npmRoot-}" ]; then
       popd
